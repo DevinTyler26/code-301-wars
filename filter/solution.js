b674ba9f520837dbee99a4ab0b91771e5addaa78
devintyler26 <devincunningham@allegropediatrics.com>
2018-01-03 19:33:58 -0800

    finished all 3

---
+++ filter/solution.js
@@ -0,0 +1,4 @@
+function filter_list(l) {
+  var numbers = l.filter(Number.isInteger);
+  return numbers;
+}
---
+++ map/solution.js
@@ -0,0 +1,4 @@
+function double(array) {
+    let numbers = array.map(num => num * 2)
+    return numbers
+}
